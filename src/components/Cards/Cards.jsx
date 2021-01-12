ebf204ee036d749b26adb1a719a8b293077d9b48
Zuzki <ldegrac071@gmail.com>
2021-01-12 11:17:47 -0500

    Implemented infinite scroll
    I implemented infinite scroll to stop the rendering of all the countries

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,26 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Card from './Card/Card'
 import { Container } from './CardsStyled'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 function Cards({ countries }) {
+
+  const [count, setCount] = useState({
+    prev: 0,
+    next: 16,
+  })
+  const [hasMore, setHasMore] = useState(true)
+  const [current, setCurrent] = useState(countries.slice(count.prev, count.next))
+  const getMoreData = () => {
+    if( current.length === countries.length ){
+      setHasMore( false );
+      return;
+    }
+    setTimeout(() => {
+      setCurrent(current.concat(countries.slice(count.prev + 10, count.next + 10)))
+    }, 2000)
+    setCount((prevState) => ({ prev: prevState.prev + 10, next: prevState.next + 10 }))
+  }
+
   return (
     <Container>
-      {countries.map( country => {
-        return(
-          <Link to={`/${country.name}`}>
-            <Card 
-              flag={ country.flag } 
-              name={ country.name } 
-              info={{ 
-                'population': country.population, 
-                'region': country.region, 
-                'capital': country.capital 
-              }}
-              key={ country.name } />
+      <InfiniteScroll
+      dataLength={current.length}
+      next={getMoreData}
+      hasMore={hasMore}
+      loader={<h2 style={{width: '100%', textAlign: 'center' }}>Loading...</h2>}
+      style={{ display: 'flex', justifyContent:'space-between', flexFlow: 'row wrap' }} >
+        {current.map( country => {
+          return(
+            <Link to={`/${country.name}`} key={country.name}>
+              <Card 
+                flag={ country.flag } 
+                name={ country.name } 
+                info={{ 
+                  'population': country.population, 
+                  'region': country.region, 
+                  'capital': country.capital 
+                }}
+                key={ country.name } />
             </Link>
-        )
-      })}
+          )
+        })}
+      </InfiniteScroll>
     </Container>
   )
 }
