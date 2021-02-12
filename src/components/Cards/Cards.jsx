487c8474bc9c85b3fd297e9e0515e19c3176deb0
Zuzki <ldegrac071@hotmail.com>
2021-02-12 11:05:17 -0500

    Fixed an issue
    Fixed an issue where countries where incorrectly updated

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Card from './Card/Card'
 import { Container } from './CardsStyled'
@@ -6,53 +6,51 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 
 function Cards({ countries }) {
 
-  const [count, setCount] = useState({
-    prev: 0,
-    next: 16,
-  })
-  const [hasMore, setHasMore] = useState(true)
-  const [current, setCurrent] = useState(countries.slice(count.prev, count.next))
-  const getMoreData = () => {
-    if( current.length === countries.length ){
-      setHasMore( false );
-      return;
-    }
-    setTimeout(() => {
-      setCurrent(current.concat(countries.slice(count.prev + 16, count.next + 16)))
-    }, 2000)
-    setCount({ ...count, prev: count.prev + 16, next: count.next + 16 })
-  }
+	const [count, setCount] = useState({
+		prev: 0,
+		next: 12,
+	})
+	const [hasMore, setHasMore] = useState(true)
+	const [current, setCurrent] = useState(countries.slice(count.prev, count.next))
+	
+	const getMoreData = () => {
+		if( current.length === countries.length ){
+			setHasMore( false );
+			return;
+		}
+		setTimeout(() => {
+			console.log( current )
+			setCurrent(current.concat(countries.slice(count.prev + 12, count.next + 12)))
+		}, 2000)
+		setCount( prevState => ({ prev: prevState.prev + 12, next: prevState.next + 12 }))
+	}
 
-  useEffect(() => {
-    setCurrent( countries.slice(count.prev, count.next) )
-  }, [countries, count])
-
-  return (
-    <Container>
-      <InfiniteScroll
-      dataLength={current.length}
-      next={getMoreData}
-      hasMore={hasMore}
-      loader={<h2 style={{width: '100%', textAlign: 'center' }}>Loading...</h2>}
-      style={{ display: 'flex', justifyContent:'space-between', flexFlow: 'row wrap' }} >
-        {current.map( country => {
-          return(
-            <Link to={`/${country.name}`} key={country.name}>
-              <Card 
-                flag={ country.flag } 
-                name={ country.name } 
-                info={{ 
-                  'population': country.population, 
-                  'region': country.region, 
-                  'capital': country.capital 
-                }}
-                key={ country.name } />
-            </Link>
-          )
-        })}
-      </InfiniteScroll>
-    </Container>
-  )
+	return (
+		<Container>
+			<InfiniteScroll
+			dataLength={current.length}
+			next={getMoreData}
+			hasMore={hasMore}
+			loader={<h2 style={{width: '100%', textAlign: 'center' }}>Loading...</h2>}
+			style={{ display: 'flex', justifyContent:'space-between', flexFlow: 'row wrap' }} >
+				{current.map( country => {
+					return(
+						<Link to={`/${country.name}`} key={country.name}>
+							<Card 
+								flag={ country.flag } 
+								name={ country.name } 
+								info={{ 
+									'population': country.population, 
+									'region': country.region, 
+									'capital': country.capital 
+								}}
+								key={ country.name } />
+						</Link>
+					)
+				})}
+			</InfiniteScroll>
+		</Container>
+	)
 }
 
 export default Cards
